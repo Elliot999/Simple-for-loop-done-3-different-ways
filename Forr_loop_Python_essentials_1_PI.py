a13ac83b5c9bd95df339fbe9ed306abac0fcccb2
Elliot999 <68709101+Elliot999@users.noreply.github.com>
2021-08-27 20:43:00 +0100

    Forr_loop_Python_essentials_1_PI.py

---
+++ Forr_loop_Python_essentials_1_PI.py
@@ -0,0 +1,22 @@
+import time
+
+for seconds in range(1,6):
+    if seconds <=4:
+        print(seconds,"Mississippi")
+        time.sleep(1)
+    else:
+         print(seconds,"Mississippi", "\n Ready or not, here I come!")
+
+for seconds in range(5):
+    seconds += 1
+    print(seconds,"Mississippi!!!!!!")
+    time.sleep(1)
+
+print(seconds,"Mississippi", "\n Ready or not, here I come!")
+
+for seconds in range(5):
+    seconds = seconds + 1
+    print(seconds,"Mississippi!!!!!!")
+    time.sleep(1)
+
+print(seconds,"Mississippi", "\n Ready or not, here I come!")
